624e4f96fcdcde7742012080f32735a144c47498
ajsalasa <abraham.salas.alfaro@intel.com>
2022-09-22 16:54:05 -0600

    Adding code

---
+++ HttpTest/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HttpTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(@"https://reqres.in/api/users?page=2");
+
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            using (Stream stream = response.GetResponseStream())
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                Console.WriteLine(reader.ReadToEnd());
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
